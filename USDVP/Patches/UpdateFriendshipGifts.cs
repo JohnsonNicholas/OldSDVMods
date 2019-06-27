d7e9379caa7f437e6dab36b5b38fe016b0eefb89
Nicholas Johnson <johnson.nicholas.h@gmail.com>
2019-06-27 12:52:14 -0700

    This is a pretty abanonded mod too. Moving it into there. I may return to it at a later date, but I probably won't, as most of the bugs are now fixed.

---
+++ USDVP/Patches/UpdateFriendshipGifts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewValley;
+
+namespace USDVP.Patches
+{
+    class UpdateFriendshipGifts
+    {
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public static bool Prefix(Farmer __instance)
+        {
+            WorldDate currDate = new WorldDate(Game1.year, Game1.currentSeason, Game1.dayOfMonth); //prop the date
+
+            foreach (var key in __instance.friendshipData.Keys)
+            {
+                if (__instance.friendshipData[key].LastGiftDate != null && currDate.TotalDays != __instance.friendshipData[key].LastGiftDate.TotalDays)
+                    __instance.friendshipData[key].GiftsToday = 0;
+                
+                // ReSharper disable once InvertIf
+                if (__instance.friendshipData[key].LastGiftDate != null && currDate.TotalWeeks != __instance.friendshipData[key].LastGiftDate.TotalWeeks)
+                {
+                    if (__instance.friendshipData[key].GiftsThisWeek == 2)
+                        __instance.friendshipData[key].Points = Math.Min(__instance.friendshipData[key].Points + 10, 2749);
+                    __instance.friendshipData[key].GiftsThisWeek = 0;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ USDVP/USDVP.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using Harmony;
+using Microsoft.Xna.Framework;
+using StardewValley;
+
+namespace USDVP
+{
+    public class USDVP : Mod
+    {
+        internal static IMonitor Logger;
+
+        internal static Type GetSDVType(string type)
+        {
+            const string prefix = "StardewValley.";
+            return Type.GetType(prefix + type + ", Stardew Valley") ?? Type.GetType(prefix + type + ", StardewValley");
+        }
+
+        public override void Entry(IModHelper helper)
+        {
+            Logger = this.Monitor;
+
+            //load up the harmony patches
+            var harmony = HarmonyInstance.Create("koihimenakamura.unofficialpatch");
+            
+            //resolve issue #7
+            Monitor.Log("Patching Farmer::updateFriendshipGifts", LogLevel.Trace);
+            var updateFriendshpGifts = AccessTools.Method(GetSDVType("Farmer"), "updateFriendshipGifts");
+            var prefixGifts = helper.Reflection.GetMethod(typeof(Patches.UpdateFriendshipGifts), "Prefix").MethodInfo;
+            harmony.Patch(updateFriendshpGifts, new HarmonyMethod(prefixGifts), null);
+
+            SaveEvents.BeforeSave += HandleEndOfDay;
+            SaveEvents.AfterLoad += SaveEvents_AfterLoad;
+
+            helper.ConsoleCommands.Add("usdvp_giftreset", "This command force resets the gifts to be 0.", GiftReset);
+        }
+
+        private void GiftReset(string arg1, string[] arg2)
+        {
+            //manual command
+            foreach (var f in Game1.player.friendshipData.Pairs)
+            {
+                f.Value.GiftsThisWeek = 0;
+            }
+        }
+
+        private void SaveEvents_AfterLoad(object sender, EventArgs e)
+        {
+            //retro fix for 1.2 saves
+            WorldDate cDate = new WorldDate(Game1.year, Game1.currentSeason, Game1.dayOfMonth -1);
+            WorldDate today = new WorldDate(Game1.year, Game1.currentSeason, Game1.dayOfMonth);
+            bool checkForManualReset = false;
+
+            foreach (var f in Game1.player.friendshipData.Pairs){
+                if (f.Value.GiftsThisWeek == 2 && f.Value.LastGiftDate == null)
+                {
+                    f.Value.LastGiftDate = cDate;
+                    checkForManualReset = true;
+                }
+
+                f.Value.GiftsToday = 0;
+            }
+
+            if (today.DayOfWeek == DayOfWeek.Monday && checkForManualReset)
+            {
+                Game1.player.updateFriendshipGifts(today);
+            }
+
+
+        }
+
+        private void HandleEndOfDay(object sender, EventArgs e)
+        {
+            Monitor.Log("Running evening fixes", LogLevel.Trace);
+            //resolve issue #10.
+            foreach (var v in Game1.player.friendshipData.Pairs)
+            {
+                if (v.Value.LastGiftDate?.Equals(new WorldDate(1, "spring", 0)) == true)
+                {
+                    v.Value.LastGiftDate = new WorldDate(Game1.year, Game1.currentSeason, (Game1.dayOfMonth -1));
+                }
+            }
+        }
+    }
+}
